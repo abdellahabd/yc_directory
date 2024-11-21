846302a1ef303395f204f4891c60cda64e22af29
abdellahabd <99611751+abdellahabd@users.noreply.github.com>
2024-11-21 11:13:57 +0100

    add write client in sanity

--- components/views.tsx
+++ components/views.tsx
@@ -1,7 +1,9 @@
 import { client } from "@/sanity/lib/client";
+import { unstable_after as after } from "next/server";
 import Ping from "./Ping";
 import { Startup_Query_Views } from "@/sanity/lib/queries";
 import { Startup } from "@/sanity/types";
+import { write_client } from "@/sanity/lib/write-client";
 
 async function Views({ id }: { id: string | undefined }) {
   const { views: TotalViews } = await client
@@ -9,6 +11,12 @@ async function Views({ id }: { id: string | undefined }) {
     .fetch(Startup_Query_Views, {
       id: id,
     });
+  after(async () => {
+    await write_client
+      .patch(id!)
+      .set({ views: TotalViews + 1 })
+      .commit();
+  });
   return (
     <div className="view-container">
       <div className="absolute -top-2 -right-2">
